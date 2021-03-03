1a4687276abf91581bd633fc33233673aef59d23
Akanksha Thokade <aka-thokade@users.noreply.github.com>
2021-03-03 12:52:00 +0530

    Create attribute_parser.cpp

---
+++ C++/attribute_parser.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, q;
+    // code here
+    return 0;
+}
