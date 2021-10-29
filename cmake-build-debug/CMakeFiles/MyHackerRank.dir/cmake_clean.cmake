3528c14cd53867930276306d2aa251fa82b37b07
Akanksha Thokade <60259783+aka-thokade@users.noreply.github.com>
2021-10-29 14:41:57 +0530

    cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/MyHackerRank.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyHackerRank.dir/InterviewPreparationKit/Sales_by_match.cpp.obj"
+  "MyHackerRank.exe"
+  "MyHackerRank.exe.manifest"
+  "MyHackerRank.pdb"
+  "libMyHackerRank.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyHackerRank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
